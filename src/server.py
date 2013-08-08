28706c082d7f6a1942c318bc37b4aa1937081a84
Marco Massenzio <m.massenzio@gmail.com>
2013-08-08 10:26:52 -0700

    Added flask-rest

--- src/server.py
+++ src/server.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 #
 # Copyright (c) 2013 RiverMeadow Software Inc. All rights reserved.
-import json
-import logging
+
 
 __author__ = 'Marco Massenzio (marco@rivermeadow.com)'
 
@@ -18,10 +17,14 @@ __author__ = 'Marco Massenzio (marco@rivermeadow.com)'
 
 import flask
 from flask import Flask, abort
+from flask.ext import restful
+import json
+import logging
 import snooper
 
 
 SECRET_KEY = '#3K5h43Hl53&s0Bod62y$%C34t6oDv3NN47Oz24GT7$3TFJWDS5yX7E7&a4994e0'
+REST_BASE_URL = '/api/1/query'
 
 # Globals are evil, but apparently there is no (easy) way around this in Flask
 # The good news is, we can keep this as a 'thin wrapper' around a truly OO design,
@@ -30,32 +33,35 @@ SECRET_KEY = '#3K5h43Hl53&s0Bod62y$%C34t6oDv3NN47Oz24GT7$3TFJWDS5yX7E7&a4994e0'
 # The downside is that we will need to create Global objects here, for all the entities that are
 # needed for each route.
 app = Flask('snooper')
+api = restful.Api(app)
 conf = snooper.parse_args()
 db_conn = snooper.DbSnooper(conf=snooper.config_connection(conf))
 
 
-@app.route('/api/1/query')
-def get_all_queries():
-    queries = []
-    for query in snooper.parse_queries(conf.queries):
-        queries.append(query)
-    return json.dumps({"queries": queries})
+class QueryAllResource(restful.Resource):
 
-@app.route('/api/1/query/<query>/<path:args>')
-def execute_query(query, args=None):
-    query = snooper.parse_queries(conf.queries).get(query)
-    if query:
-        db_conn.query = query.get("sql")
-        db_conn.params = snooper.parse_query_params(args.split('/'))
-        app.logger.debug('Executing query: {} - with params: {}'.format(db_conn.query,
-                                                                        db_conn.params))
-        res = db_conn.execute()
-        app.logger.debug('Found {} results'.format(res["rowcount"]))
-        res["drill_down"] = query.get("drill_down")
-        return json.dumps(res)
-    else:
-        app.logger.error('Could not find query %s', query)
-        abort(404)
+    def get(self):
+        queries = []
+        for query in snooper.parse_queries(conf.queries):
+            queries.append(query)
+        return {"queries": queries}
+
+
+class QueryResource(restful.Resource):
+    def get(self, query, args=None):
+        query = snooper.parse_queries(conf.queries).get(query)
+        if query:
+            db_conn.query = query.get("sql")
+            db_conn.params = snooper.parse_query_params(args.split('/'))
+            app.logger.debug('Executing query: {} - with params: {}'.format(db_conn.query,
+                                                                            db_conn.params))
+            res = db_conn.execute()
+            app.logger.debug('Found {} results'.format(res["rowcount"]))
+            res["drill_down"] = query.get("drill_down")
+            return res
+        else:
+            app.logger.error('Could not find query %s', query)
+            abort(404)
 
 if __name__ == '__main__':
     if not conf.debug:
@@ -63,4 +69,6 @@ if __name__ == '__main__':
         # we enable it here to log DEBUG level to Console
         app.logger.setLevel(logging.DEBUG)
         app.logger.addHandler(logging.StreamHandler())
+    api.add_resource(QueryAllResource, REST_BASE_URL)
+    api.add_resource(QueryResource, '/'.join([REST_BASE_URL, '<query>', '<path:args>']))
     app.run(debug=conf.debug)
